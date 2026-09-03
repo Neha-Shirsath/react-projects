302f7eb01509749a858dd464be3134830c3b5c4d
Neha Shirsath <nehashirsath308@gmail.com>
2026-09-03 19:02:28 +0530

    "Ignited react-app"

--- app.js
+++ app.js
@@ -9,11 +9,12 @@
 //         <h2>Welcome back!!</h2>
 //     </div>
 // </div>
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", {id:"parent"}, 
         [React.createElement("div", {id:"child1"}, 
-        [React.createElement("h1", {}, "Hello h1"),
+        [React.createElement("h1", {}, "Hello h1, I m using Parcel🚀"),
         React.createElement("h2", {}, "Welcome back!!")]),
 
         React.createElement("div", {id:"child2"}, 
--- readme.md
+++ readme.md
@@ -1,1 +1,18 @@
 # React Projects
+
+## Parcel
+- dev build
+- local server
+- HMR(Hot Module Replacement) > auto updates
+- file Watching algorithm (in c++)
+- Faster builds > caching
+- Image Optimization
+- bundling
+- dev and prod bundle
+- compressing
+- code splitting
+- differential bundling(supports older browsers)
+- diagnostic
+- Error handling
+- HTTPs
+- Tree shaking(removes unused code from app)
