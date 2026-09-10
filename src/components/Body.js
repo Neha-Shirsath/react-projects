6cb02a2033fa8a8474fd227d51c49dec3b04402a
Neha Shirsath <nehashirsath308@gmail.com>
2026-09-10 11:01:27 +0530

    hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,22 +1,25 @@
 import ResCard from "./restaurantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Appbody = () => {
+
+    const [listOfRes, setListOfRes] = useState(resList);
+    
     return (
         <div className="app-body">
             <div className="searching">
-                <div className="search-container"> <input className="search" id="search" type="text" placeholder="Search" /></div>
+                <div className="search-container"> <input className="search" id="search" type="text" placeholder="Search for restaurant, cuisine or a dish" /></div>
             <div className="filter">
-                <button className="top-res" onClick={() => {
-                    console.log(resList.filter((res) => res.info.rating.aggregate_rating > 4 ))
-
-                    
+                <button className="top-res" onClick={() => { 
+                    const filteredList = listOfRes.filter((res) => (res.info.rating.aggregate_rating > 4));
+                    setListOfRes(filteredList);
                 }}>Top Restaurants</button>
             </div>
             </div>
               <div className="res-container">
                 {
-                  resList.map((restaurant) => (<ResCard key={restaurant.info.resId} resData = {restaurant}/>))
+                  listOfRes.map((restaurant) => (<ResCard key={restaurant.info.resId} resData = {restaurant}/>))
                 }
               </div>
         </div>
