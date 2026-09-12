d3940a18b307b3a114959b4e450bbe9ca055644d
Neha Shirsath <nehashirsath308@gmail.com>
2026-09-12 13:11:36 +0530

    searchCom

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,7 @@
 import ResCard from "./restaurantCard";
 import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
+import { SEARCH_ICON } from "../utils/constants"
 
 const Appbody = () => {
 
@@ -25,7 +26,12 @@ const Appbody = () => {
     return listOfRes.length === 0 ? (<Shimmer/>) : (
         <div className="app-body">
             <div className="searching">
-                <div className="search-container"> <input className="search" id="search" type="text" placeholder="Search for restaurant, cuisine or a dish" /></div>
+                <div className="search-container">
+                    {/* <img src= {SEARCH_ICON} /> */}
+                    <input className="search" id="search" type="text" placeholder="Search for restaurant, cuisine or a dish" />
+                    <button className="search-btn">Search</button>
+                </div>
+
             <div className="filter">
                 <button className="top-res" onClick={() => { 
                     const filteredList = listOfRes.filter((res) => (res.info.avgRating > 4.2));
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
 export const LOGO_URL = "https://img.freepik.com/premium-vector/burger-vector-illustration-flat-hamburger-icon-logo-design_1058532-20232.jpg"
 
 export const IMG_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const SEARCH_ICON = "https://p.kindpng.com/picc/s/62-626977_icon-gambar-search-hd-png-download.png"
